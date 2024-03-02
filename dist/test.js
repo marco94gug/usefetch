8636651e639461ac5c8e7801c4904362e126275a
marco94gug <marco.guglielmino94@gmail.com>
2024-03-02 23:29:28 +0100

    feat: test 2

---
+++ dist/test.js
@@ -0,0 +1,6 @@
+"use strict";
+const foo = "bar";
+const foobarFn = (bar) => {
+    return `foo${bar}`;
+};
+foobarFn("bar");
---
+++ src/test.ts
@@ -0,0 +1,7 @@
+const foo: string = "bar";
+
+const foobarFn = (bar: string): string => {
+  return `foo${bar}`;
+};
+
+foobarFn("bar");
